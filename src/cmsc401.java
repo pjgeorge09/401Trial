f28acd3b3aeb0224278c9302e56c480000364a00
Peter George <peter.justin.george@gmail.com>
2019-01-19 10:30:38 -0500

    I made one change, added text test

--- src/cmsc401.java
+++ src/cmsc401.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class cmsc401 {
-	public static void main(String[] args){	
+	public static void main(String[] args){	 //If I make a change
 		
 	Scanner sc = new Scanner(System.in); //initializing scanner
 	String input = sc.nextLine(); //Get the first line from user
